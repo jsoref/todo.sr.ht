1b2f0a6c9704428c66a5541a316f1d5d7e6b61fe
Drew DeVault <sir@cmpwn.com>
2017-09-19 18:24:17 -0400

    Add username to todo.sr.ht breadcrumbs

--- todosrht/app.py
+++ todosrht/app.py
@@ -48,6 +48,7 @@ def tracker_name(tracker, full=False):
     user = "~" + tracker.owner.username
     if full:
         return Markup(
+            "<a href='/{0}'>{0}</a>/".format(user) +
             "/".join([
                 "<a href='/{}/{}'>{}</a>".format(user, "/".join(split[:i + 1]), p)
                 for i, p in enumerate(split)
@@ -57,6 +58,7 @@ def tracker_name(tracker, full=False):
         return name
     parts = split[:-1]
     return Markup(
+        "<a href='/{0}'>{0}</a>/".format(user) +
         "/".join([
             "<a href='/{}/{}'>{}</a>".format(user, "/".join(parts[:i + 1]), p)
             for i, p in enumerate(parts)
