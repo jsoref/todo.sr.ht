ea1dff6aea9954692b25e23c22b747791108b69b
Drew DeVault <sir@cmpwn.com>
2017-11-09 22:29:34 -0500

    Add profile info to user pages

--- todosrht/blueprints/html.py
+++ todosrht/blueprints/html.py
@@ -2,15 +2,19 @@ from flask import Blueprint, render_template
 from flask_login import current_user
 from todosrht.types import Tracker, Event, EventNotification, EventType
 from todosrht.types import User
+from srht.config import cfg
+import requests
 
 html = Blueprint('html', __name__)
+
+meta_uri = cfg("network", "meta")
 
 @html.route("/")
 def index():
     if not current_user:
         return render_template("index.html")
     # TODO: pagination?
-    your_trackers = (Tracker.query
+    trackers = (Tracker.query
             .filter(Tracker.owner_id == current_user.id)
             .order_by(Tracker.updated.desc())).all()
     events = [e.event for e in (EventNotification.query
@@ -36,8 +40,17 @@ def user_GET(username):
             .filter(Event.user_id == user.id)
             .order_by(Event.created.desc())
             .limit(10)).all()
+    r = requests.get(meta_uri + "/api/user/profile", headers={
+        "Authorization": "token " + user.oauth_token
+    }) # TODO: cache this
+    if r.status_code == 200:
+        profile = r.json()
+    else:
+        profile = None
     return render_template("dashboard.html",
+            user=user,
+            profile=profile,
             trackers=trackers,
-            tracker_list_msg="{}'s Trackers".format(user.username),
+            tracker_list_msg="Trackers".format(user.username),
             events=events,
             EventType=EventType)
