ea8056cfa77746bc56ed6e2ee3033904ab0cfbbc
Drew DeVault <sir@cmpwn.com>
2022-01-04 15:49:18 +0100

    API: rehome email notification templates

--- api/graph/resolver.go
+++ api/graph/resolver.go
@@ -2,7 +2,6 @@ package graph
 
 import (
 	"regexp"
-	"text/template"
 
 	"git.sr.ht/~sircmpwn/todo.sr.ht/api/graph/model"
 )
@@ -13,29 +12,7 @@ type Resolver struct{}
 
 var (
 	trackerNameRE   = regexp.MustCompile(`^[A-Za-z0-9._-]+$`)
-	newTicketTemplate = template.Must(template.New("new-ticket").Parse(`{{.Body}}
-
--- 
-View on the web: {{.Root}}{{.TicketURL}}`))
-	ticketStatusTemplate = template.Must(template.New("ticket-status").Parse(`{{if eq .Status "RESOLVED"}}Ticket resolved: {{.Resolution}}{{end}}
-
--- 
-View on the web: {{.Root}}{{.TicketURL}}#event-{{.EventID}}`))
 )
-
-type NewTicketDetails struct {
-	Body      *string
-	Root      string
-	TicketURL string
-}
-
-type TicketStatusDetails struct {
-	Root       string
-	TicketURL  string
-	EventID    int
-	Status     string
-	Resolution string
-}
 
 func aclBits(input model.ACLInput) uint {
 		var bits uint
